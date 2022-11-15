434761b2e948477158faecb024486bacf82fb1a7
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2022-11-15 11:32:09 +0530

    Time: 37 ms (86.98%), Space: 13.9 MB (19.96%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,4 +1,31 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        # product = 1
+#         if(n ==0):
+#             return 1
+        
+#         pow = abs(n)
+       
+#         for i in range(pow):
+#             product = product*x
+#         if(n<0) :
+#             return (1/product)
+#         return product
+
+        
+        #Deal with negative power:
+        if n < 0: 
+            x = 1/x
+            n = abs(n)
+            
+        #Iterate:
+        res = 1
+        while n!=0:
+            if (n % 2!=0):
+                res = res*x
+            x = x*x 
+            n = n//2
+            
+        return res
+
         
