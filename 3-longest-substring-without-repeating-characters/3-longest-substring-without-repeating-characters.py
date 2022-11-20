23d31a9756f637145d2012d4e0adab21ea76e9aa
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2022-11-20 23:58:24 +0530

    Time: 81 ms (84.90%), Space: 14.3 MB (5.25%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,68 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+#         i=0
+#         j=1 
+#         if len(s) == 1:
+#             return 1
+#         l = 0 
+#         d = {}
+#         maxi = 0
+#         while(i<j):
+#             d[s[i]] = i
+#             if(s[j] in d):
+#                 d.clear()
+#                 maxi = max(maxi,j-i)
+#                 i+=1
+#                 j+=1
+#             else:
+#                 d[s[j]] = i 
+#                 j+=1
+                
+#         return maxi
+
+
+#         d = set()
+#         l,r =0,0
+#         max1 = 0 
+#         for r in range(len(s)):
+#             if(s[r] in d != list(d)[-1]):
+#                 while(l<r and s[r] != list(d)[-1]):
+#                     d.remove(s[l])
+#                     l+=1
+#             d.add(s[r])
+                
+#             max1 = max(max1,r-l+1)
+            
+            
+                
+                    
+                
+                
+#         return max1
+                
+        l = len(s)
+        if l < 2:
+            return l 
+
+        res = 0
+        i = 0 
+        for j in range(1, len(s)):
+            if s[j] in s[i:j]: # duplicate found !
+                print (s[i:j])
+                res = max(res, len(s[i:j]))
+                while s[j] in s[i:j]: # search new beginning of substring
+                    i += 1
+            elif j == len(s) - 1: # special for the last char
+                res = max(res, len(s[i:j + 1]))
+        return res  
+                
+                
+                
+            
+            
+            
+            
+            
+        
+        
