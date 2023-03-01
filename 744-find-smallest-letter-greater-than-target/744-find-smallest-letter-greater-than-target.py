ce80b00d452ccb0d932d23824388243dd513f326
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-03-02 00:21:16 +0530

    Time: 114 ms (57.89%), Space: 14.3 MB (73.43%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,26 +1,26 @@
-# class Solution:
-#     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-#         st = 0 
-#         end = len(letters) -1 
-#         while(st<end):
-            
-#             mid = st + ((end-st)//2)
-#             if target >= letters[int(mid)]:
-                
-#                 st = mid +1 
-#             else :
-#                 end = mid 
-           
-#         return letters[st%len(letters)]
-    
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        l=0
-        r=len(letters)
-        while l<r:
-            mid=(int)(l+(r-l)/2)
-            if letters[mid]<=target:
-                l=mid+1
-            else:
-                r=mid
-        return letters[l%len(letters)]
+        st = 0 
+        end = len(letters) 
+        while(st<end):
+            
+            mid = st + ((end-st)//2)
+            if target >= letters[int(mid)]:
+                
+                st = mid +1 
+            else :
+                end = mid 
+           
+        return letters[st%len(letters)]
+    
+# class Solution:
+#     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+#         l=0
+#         r=len(letters)
+#         while l<r:
+#             mid=(int)(l+(r-l)/2)
+#             if letters[mid]<=target:
+#                 l=mid+1
+#             else:
+#                 r=mid
+#         return letters[l%len(letters)]
