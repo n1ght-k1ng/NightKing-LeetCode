2f67fad4a023890ec720fff61cac62e2bb4bee33
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-03-13 22:00:22 +0530

    Time: 589 ms (94.57%), Space: 27.8 MB (28.16%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,9 +1,25 @@
-class Solution:        
+class Solution:    
+    
+    
+    #     flag = 0
+    #     for i in range(len(arr)-1):
+    #         if arr[i] > arr[i+1]:
+    #             flag = i
+    #             break
+    #     return flag
+    #    -- Brute Force Approach --
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        flag = 0
-        for i in range(len(arr)-1):
-            if arr[i] > arr[i+1]:
-                flag = i
+        s = 0 
+        e = len(arr)-1
+        while(s<=e):
+            mid = s + ((e-s)//2)
+            if arr[mid] > arr[mid+1]:
+                e = mid
+            else:
+                
+                s= mid+1
+            if s == e:
                 break
-        return flag
+        print(s)
+        return s
         
