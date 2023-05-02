e362c437722cff2c7ee2efbfcd86bfffe27fb8c1
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-05-02 23:40:12 +0530

    Time: 2 ms (88.78%), Space: 40.5 MB (62.14%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        if (s.length() == 1)
+            return false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+            
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+        
+        }
+        
+    }
