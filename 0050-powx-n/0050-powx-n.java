b17c6767386c3b98fd2eb68e4315edbfa6e1954b
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-09-16 14:20:51 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (98.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        return powres(x, n);
+    }
+
+    public double powres(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double half = powres(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
