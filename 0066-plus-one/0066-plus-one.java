2caa07eb630bcb1eb92412896823794d292ef3dc
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-12-08 23:31:35 +0530

    Time: 1 ms (6.87%), Space: 40.7 MB (69.51%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public int[] plusOne(int[] digits) {
+        List<Integer> res= new ArrayList<>();
+        for (int value : digits) {
+            res.add(value);
+        }
+        
+        for(int i = res.size()-1 ; i >=0 ; i--){
+            if(res.get(i)!=9){
+                int temp = res.get(i);
+                res.set(i,temp+1);
+                break;
+            }else{
+                res.set(i,0);
+            }
+        }
+        
+        if(res.get(0) == 0 && digits[0] != 0){
+            res.add(0,1);
+        }
+        int[] ret = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            ret[i] = res.get(i);
+        }
+        return ret;
+    }
+}
