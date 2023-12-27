d0ab9b41c901c506b814c08c11c8379eef4688bb
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-12-27 17:02:34 +0530

    Time: 104 ms (7.33%), Space: 46.8 MB (6.26%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        char[] charArray1 = word1.toCharArray();
+        char[] charArray2 = word2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        word1 = new String(charArray1);
+        word2 = new String(charArray2);
+        System.out.println(word1 + " " + word2);
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        
+        for (char c : word1.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        for (char c : word2.toCharArray()) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+        
+        List<Character> ar1 = new ArrayList<>(map1.keySet());
+        List<Character> ar2 = new ArrayList<>(map2.keySet());
+        
+        List<Integer> arr1 = new ArrayList<>(map1.values());
+        List<Integer> arr2 = new ArrayList<>(map2.values());
+        
+        Collections.sort(arr1);
+        Collections.sort(arr2);
+        Collections.sort(ar1);
+        Collections.sort(ar2);
+        
+        return arr1.equals(arr2) && ar1.equals(ar2);
+        
+    
+    }
+}
