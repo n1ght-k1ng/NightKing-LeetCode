32f6d3c32f62acac8f8a527f589928dd61e46af4
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2023-12-30 13:08:12 +0530

    Time: 22 ms (5.25%), Space: 84.1 MB (5.09%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         if(preorder.length == 0){
+//             return null;
+//         }
+        
+//         int root = preorder[0];
+//         int i = 0;
+//         int index =0 ;
+//         for(i= 0 ; i < inorder.length; i++){
+//             if(inorder[i] == root){
+//                 index = i;
+//             }
+//         }
+        
+//         TreeNode node = new TreeNode(root);
+//         node.left = buildTree(Arrays.copyOfRange(preorder,1,index +1) , Arrays.copyOfRange(inorder, 0 , index));
+//         node.right = buildTree(Arrays.copyOfRange(preorder,index +1 ,preorder.length) , Arrays.copyOfRange(inorder, index+1, inorder.length));
+//         return node;
+// }
+// }
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        
+        if(inorder.length == 0){
+            return null;
+        }
+        
+        int root = postorder[postorder.length -1];
+        int index = 0; 
+        for(int i =0 ; i < inorder.length ; i++){
+            if(inorder[i] == root){
+                index = i;
+            }
+        }
+        
+        TreeNode node = new TreeNode(root);
+        node.left = buildTree(Arrays.copyOfRange(inorder ,0 , index), Arrays.copyOfRange(postorder , 0 , index));
+        node.right = buildTree(Arrays.copyOfRange(inorder , index +1 , inorder.length), Arrays.copyOfRange(postorder , index , postorder.length -1 ));
+        return node;
+        
+        
+    }
+}
