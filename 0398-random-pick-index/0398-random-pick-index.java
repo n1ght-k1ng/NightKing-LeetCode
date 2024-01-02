625472cf976c477a11a928adee483d9647f18648
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2024-01-02 14:07:31 +0530

    Time: 77 ms (66.18%), Space: 56.6 MB (7.27%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -0,0 +1,29 @@
+class Solution {
+    Map<Integer, List<Integer>> map;
+    static Random random = new Random();
+
+    public Solution(int[] nums) {
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i];
+            if (map.containsKey(key)) {
+                map.get(key).add(i);
+            } else {
+                List<Integer> ids = new ArrayList<>();
+                ids.add(i);
+                map.put(key, ids);
+            }
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> ids = map.get(target);
+        return ids.get(random.nextInt(ids.size()));
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
