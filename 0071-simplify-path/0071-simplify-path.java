d410c74af5344c417bd77b3f7bdee5438ba84465
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2024-01-16 14:22:55 +0530

    Time: 4 ms (87.04%), Space: 43.1 MB (75.41%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String s) {
+        Stack<String> stack = new Stack<>();
+        String[] components = s.split("/");
+
+        for (String component : components) {
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!component.isEmpty() && !component.equals(".")) {
+                stack.push(component);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
